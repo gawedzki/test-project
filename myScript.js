d6860f0c5616bb0544200fbda0a8b44ecaea027c
Pawel <pawelgawedzki@gmail.com>
2021-01-14 21:02:09 +0100

    Add options to add/remove cells/rows

--- myScript.js
+++ myScript.js
@@ -1,87 +1,114 @@
 var exercisesList = ['Exercise1', 'Exercise2', 'Exercise3', 'Exercise4', 'Exercise5'];
             
-            var exercise = {
-                name: exercisesList,
-                sets: 4,
-                reps: [[40, 8], [50, 7], [60, 6]]
-            }
+var exercise = {
+    name: exercisesList,
+    sets: 4,
+    reps: [[40, 8], [50, 7], [60, 6]]
+}
 
-            var rows = 0;
+function createNewTable() {
+    var row = document.createElement("tr");
+    var exerciseCell = document.createElement("th");
+    exerciseCell.innerHTML = "Exercise:";
 
-            function createNewTable() {
-                var row = document.createElement("tr");
-                var exerciseCell = document.createElement("th");
-                exerciseCell.innerHTML = "Exercise:";
+    row.appendChild(exerciseCell);
 
-                row.appendChild(exerciseCell);
+    for (var i = 0; i < exercise.sets; i++) {
+        addHeadCells(row);
+    }
 
-                for (var i = 0; i < exercise.sets; i++) {
-                    var weightCell = document.createElement("th");
-                    var repCell = document.createElement("th");
-                    weightCell.innerHTML = "Kg:"
-                    repCell.innerHTML = "Reps:";
-                    row.appendChild(weightCell);
-                    row.appendChild(repCell);
-                }
-                var element1 = document.getElementById("myTable");
-                element1.appendChild(row);
+    var element1 = document.getElementById("myTable");
+    element1.appendChild(row);
 
-                var addExerciseButton = document.createElement("button");
-                addExerciseButton.innerHTML = "Add exercise!";
-                addExerciseButton.onclick = function(){ addRow(); };
+    var addExerciseButton = document.createElement("button");
+    addExerciseButton.innerHTML = "Add exercise!";
+    addExerciseButton.onclick = function(){ addRow(); };
 
-                var addSetButton = document.createElement("button");
-                addSetButton.innerHTML = "Add set!";
-                addSetButton.onclick = function() { addSet(); };
+    var removeExerciseButton = document.createElement("button");
+    removeExerciseButton.innerHTML = "Remove exercise!";
+    removeExerciseButton.onclick = function() { removeRow(); };
 
-                var removeSetButton = document.createElement("button");
-                removeSetButton.innerHTML = "Remove set!";
-                removeSetButton.onclick = function() { removeSet(); }
+    var addSetButton = document.createElement("button");
+    addSetButton.innerHTML = "Add set!";
+    addSetButton.onclick = function() { addSet(); };
 
-                var element2 = document.getElementById("tableDiv");
-                element2.appendChild(addExerciseButton);
-                element2.appendChild(addSetButton);
-                element2.appendChild(removeSetButton);
-            }
+    var removeSetButton = document.createElement("button");
+    removeSetButton.innerHTML = "Remove set!";
+    removeSetButton.onclick = function() { removeSet(); }
 
-            function addSet() {
-                // exercise.sets++;
-            }
+    var element2 = document.getElementById("tableDiv");
+    element2.appendChild(addExerciseButton);
+    element2.appendChild(removeExerciseButton);
+    element2.appendChild(addSetButton);
+    element2.appendChild(removeSetButton);
+}
 
-            function removeSet() {
-                // if(exercise.sets > 1) {
-                //     exercise.sets--;
-                // }
-            }
+function addSet() {
+    var rows = document.getElementsByTagName("tr");
 
-            function addRow() {
-                var row = document.createElement("tr");
-                var exerciseCell = document.createElement("td");
+    addHeadCells(rows[0]);
 
-                var chooseExercise = document.createElement("select");
-                for(var i = 0; i <exercisesList.length; i++) {
-                    chooseExercise.options[i] = new Option(exercisesList[i], exercisesList[i]);
-                }
-                exerciseCell.appendChild(chooseExercise);
+    for(var i = 1; i < rows.length; i++) {
+        addBodyCells(rows[i]);
+    }
 
-                row.appendChild(exerciseCell);
+    exercise.sets++;
+}
 
-                for(var i = 0; i < exercise.sets; i++) {
-                    var weightCell = document.createElement("td");
-                    var weightData = document.createElement("input");
-                    weightData.type = "text";
-                    weightCell.appendChild(weightData);
-                    
-                    var repCell = document.createElement("td");
-                    var repData = document.createElement("input");
-                    repData.type = "text";
-                    repCell.appendChild(repData);
+function removeSet() {
+    var rows = document.getElementsByTagName("tr");
+    for(var i = 0; i < exercise.sets; i++) {
+        rows[i].deleteCell(-1);
+        rows[i].deleteCell(-1);
+    }
+    exercise.sets--;
+}
 
-                    row.appendChild(weightCell);
-                    row.appendChild(repCell);
-                }
-                var element = document.getElementById("myTable");
-                element.appendChild(row);
-                rows++;
-                //console.log(exercise.sets, rows);
-            }
+function addRow() {
+    var row = document.createElement("tr");
+    var exerciseCell = document.createElement("td");
+
+    var chooseExercise = document.createElement("select");
+    for(var i = 0; i <exercisesList.length; i++) {
+        chooseExercise.options[i] = new Option(exercisesList[i], exercisesList[i]);
+    }
+    exerciseCell.appendChild(chooseExercise);
+
+    row.appendChild(exerciseCell);
+
+    for(var i = 0; i < exercise.sets; i++) {
+        addBodyCells(row);
+    }
+
+    var element = document.getElementById("myTable");
+    element.appendChild(row);
+}
+
+function removeRow() {
+    var rows = document.getElementsByTagName("tr");
+    document.getElementById("myTable").deleteRow(rows.length-1);
+}
+
+function addHeadCells(row) {
+    var weightCell = document.createElement("th");
+    var repCell = document.createElement("th");
+    weightCell.innerHTML = "Kg:"
+    repCell.innerHTML = "Reps:";
+    row.appendChild(weightCell);
+    row.appendChild(repCell);
+}
+
+function addBodyCells(row) {
+    var weightCell = document.createElement("td");
+    var weightData = document.createElement("input");
+    weightData.type = "text";
+    weightCell.appendChild(weightData);
+    
+    var repCell = document.createElement("td");
+    var repData = document.createElement("input");
+    repData.type = "text";
+    repCell.appendChild(repData);
+
+    row.appendChild(weightCell);
+    row.appendChild(repCell);
+}
