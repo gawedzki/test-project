ac27df89f0c3192ae41ffce96e8617ae5bfe4358
Pawel <pawelgawedzki@gmail.com>
2021-01-15 22:34:21 +0100

    Fix display order

--- myScript.js
+++ myScript.js
@@ -2,11 +2,58 @@ var exercisesList = ['Exercise1', 'Exercise2', 'Exercise3', 'Exercise4', 'Exerci
             
 var exercise = {
     name: exercisesList,
-    sets: 4,
+    sets: 5,
     reps: [[40, 8], [50, 7], [60, 6]]
 }
 
 function createNewTable() {
+    var element = document.getElementById("tableDiv");
+
+    var addExerciseSpan = document.createElement("span");
+
+    var addText1 = document.createElement("span");
+    addText1.innerHTML = "Number of exercises: ";
+    addExerciseSpan.appendChild(addText1);
+    
+    var removeExerciseButton = document.createElement("button");
+    removeExerciseButton.innerHTML = "-";
+    removeExerciseButton.onclick = function() { removeRow(); };
+    removeExerciseButton.className = "tableButtons";
+    addExerciseSpan.appendChild(removeExerciseButton);
+
+    var addExerciseButton = document.createElement("button");
+    addExerciseButton.innerHTML = "+";
+    addExerciseButton.onclick = function(){ addRow(); };
+    addExerciseButton.className = "tableButtons";
+    addExerciseSpan.appendChild(addExerciseButton);
+
+    element.appendChild(addExerciseSpan);
+
+    var addSetsSpan = document.createElement("span");
+    
+    var addText2 = document.createElement("span");
+    addText2.innerHTML = "Number of sets: ";
+    addSetsSpan.appendChild(addText2);
+
+    var removeSetButton = document.createElement("button");
+    removeSetButton.innerHTML = "-";
+    removeSetButton.onclick = function() { removeSet(); };
+    removeSetButton.className = "tableButtons";
+    addSetsSpan.appendChild(removeSetButton);
+
+    var addSetButton = document.createElement("button");
+    addSetButton.innerHTML = "+";
+    addSetButton.onclick = function() { addSet(); };
+    addSetButton.className = "tableButtons";
+    addSetsSpan.appendChild(addSetButton);
+
+    element.appendChild(addSetsSpan);
+
+    var newTable = document.createElement("table");
+    newTable.id = "myTable";
+
+    element.appendChild(newTable)
+
     var row = document.createElement("tr");
     var exerciseCell = document.createElement("th");
 
@@ -17,30 +64,8 @@ function createNewTable() {
         addHeadCells(row);
     }
 
-    var element1 = document.getElementById("myTable");
-    element1.appendChild(row);
-
-    var addExerciseButton = document.createElement("button");
-    addExerciseButton.innerHTML = "Add exercise!";
-    addExerciseButton.onclick = function(){ addRow(); };
-
-    var removeExerciseButton = document.createElement("button");
-    removeExerciseButton.innerHTML = "Remove exercise!";
-    removeExerciseButton.onclick = function() { removeRow(); };
-
-    var addSetButton = document.createElement("button");
-    addSetButton.innerHTML = "Add set!";
-    addSetButton.onclick = function() { addSet(); };
-
-    var removeSetButton = document.createElement("button");
-    removeSetButton.innerHTML = "Remove set!";
-    removeSetButton.onclick = function() { removeSet(); };
-
-    var element2 = document.getElementById("tableDiv");
-    element2.appendChild(addExerciseButton);
-    element2.appendChild(removeExerciseButton);
-    element2.appendChild(addSetButton);
-    element2.appendChild(removeSetButton);
+    newTable.appendChild(row);
+    addRow();
 }
 
 function addSet() {
@@ -61,9 +86,10 @@ function removeSet() {
             rows[i].deleteCell(cells-1);
             rows[i].deleteCell(cells-2);
         }
+        exercise.sets--;
+    }else {
+        alert("Do at least one ;)")
     }
-
-    exercise.sets--;
 }
 
 function addRow() {
@@ -88,7 +114,7 @@ function addRow() {
 
 function removeRow() {
     var rows = document.getElementsByTagName("tr");
-    if(rows.length > 1) {
+    if(rows.length > 2) {
         document.getElementById("myTable").deleteRow(rows.length-1);
     }
 }
